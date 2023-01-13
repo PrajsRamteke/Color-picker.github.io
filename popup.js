4dc2815a4d24a000095e83279f6f036643c11a71
Prajwal <prajwalramteke9@gmail.com>
2023-01-13 09:39:32 +0530

    1st commit this first trial

---
+++ popup.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    var color = document.getElementById("color-picker").value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+    document.getElementById("hex-code").innerText = color;
+}
